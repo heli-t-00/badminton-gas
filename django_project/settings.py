0fdbeea6b0cc3b21dea7ceb2e85983266771a9dc
heli-t-00 <helen2to@gmail.com>
2025-02-25 15:42:59 +0000

    created home page with some basic info

--- django_project/settings.py
+++ django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4277)2-%$fna(o-i8(j1b^+9wf*zgsz^+m@tz!ea1b0z+$bm-8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+# # For heroku only
+DEBUG = False
 ALLOWED_HOSTS = ['.herokuapp.com']
+
+# For local only
+# DEBUG = True
+# ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -127,10 +134,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'accounts', 'static'), 
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
